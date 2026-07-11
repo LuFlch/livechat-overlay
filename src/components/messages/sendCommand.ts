4e5061b53e498c1419dc39373f9a1d7ea7faa744
jere <piresjeremie.mailpro@gmail.com>
2026-07-11 15:24:32 +0200

    fix: remove MessageFlags non used

--- src/components/messages/sendCommand.ts
+++ src/components/messages/sendCommand.ts
@@ -1,4 +1,4 @@
-import { ChatInputCommandInteraction, EmbedBuilder, MessageFlags, SlashCommandBuilder } from 'discord.js';
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { QueueType } from '../../services/prisma/loadPrisma';
 import { getContentInformationsFromUrl } from '../../services/content-utils';
 import { getDurationFromGuildId } from '../../services/utils';
